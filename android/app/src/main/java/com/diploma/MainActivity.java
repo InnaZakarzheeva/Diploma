cd304be64ca226339e45b34130bd621be34b955f
Inna <inna@Innas-Mac-mini.local>
2020-08-25 13:26:51 +0300

    prevent screenshot in app [android]

--- android/app/src/main/java/com/diploma/MainActivity.java
+++ android/app/src/main/java/com/diploma/MainActivity.java
@@ -1,6 +1,8 @@
 package com.diploma;
 
 import com.facebook.react.ReactActivity;
+import android.view.WindowManager;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +13,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "diploma";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getWindow().setFlags(
+      WindowManager.LayoutParams.FLAG_SECURE,
+      WindowManager.LayoutParams.FLAG_SECURE
+    );
   }
 }
